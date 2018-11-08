d9d67f631ab08cdea487656220390decf154f406
Wahyu Marta Dwi Maharani <44812057+Martaaadm@users.noreply.github.com>
2018-11-08 15:01:06 +0700

    Add files via upload

---
+++ Matriks.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	int mtrk[100][100];
+	int n;
+	cout<<"Masukan panjang matriks : ";
+	cin>>n;
+	cout<<endl;
+	
+	for(int k=0; k<n; k++){
+		for(int l=0; l<n; l++){
+			cout<<"Masukan ["<<k<<"]["<<l<<"] : ";
+			cin>>mtrk[k][l];
+			if(k+l==n-1)
+			{
+				mtrk[k][l]=0;
+			}
+		}
+	}
+	cout<<endl;
+	for(int k=0; k<n; k++){
+		for(int l=0; l<n; l++){
+			cout<<setw(5)<<mtrk[k][l];
+		}
+		cout<<endl;
+	}
+	return 0;
+}
